42085700cdd91782d5519a4f37a74d1da68dbf87
yujin9907 <heoyujin07@gmail.com>
2022-11-04 18:22:41 +0900

    test : h2 db로 수정

--- src/main/resources/truncate.sql
+++
@@ -1,9 +0,0 @@
-SET REFERENTIAL_INTEGRITY FALSE;
-truncate table user;
-truncate table category;
-truncate table company;
-truncate table resume;
-truncate table recruit;
-truncate table apply;
-truncate table subscribe;
-SET REFERENTIAL_INTEGRITY TRUE;
