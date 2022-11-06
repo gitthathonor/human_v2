2ab99c89f3e8b401e42432e1287013cae50a1ae8
Sylar <kmc42692@naver.com>
2022-11-06 13:13:39 +0900

    resume : 토큰필터 테스트 전으로 수정

--- src/main/java/site/metacoding/humancloud/config/auth/JwtAuthenticationFilter.java
+++ src/main/java/site/metacoding/humancloud/config/auth/JwtAuthenticationFilter.java
@@ -81,7 +81,7 @@ public class JwtAuthenticationFilter implements Filter {
                 .withExpiresAt(expire)
                 .withClaim("id", usernamePS.get().getId())
                 .withClaim("username", usernamePS.get().getUsername())
-                .withClaim("role", 1)
+                .withClaim("role", usernamePS.get().getRole())
                 .sign(Algorithm.HMAC512("3조"));
 
         // JWT토큰 응답
